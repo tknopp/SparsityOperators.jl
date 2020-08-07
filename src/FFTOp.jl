7fb2e937348fcb6d7e280dc6c09d4c9d9f452535
migrosser <mi.grosser@uke.de>
2020-08-07 16:37:40 +0200

    add dedicated type for FFTOp

--- src/FFTOp.jl
+++ src/FFTOp.jl
@@ -1,7 +1,25 @@
 export FFTOp
+import Base.copy
+
+mutable struct FFTOp{T} <: AbstractLinearOperator{T}
+  nrow :: Int
+  ncol :: Int
+  symmetric :: Bool
+  hermitian :: Bool
+  prod :: Function
+  tprod :: Nothing
+  ctprod :: Function
+  nprod :: Int
+  ntprod :: Int
+  nctprod :: Int
+  plan
+  iplan
+  shift::Bool
+  unitary::Bool
+end
 
 """
-  FFTOp(T::Type, shape::Tuple, shift=true)
+  FFTOp(T::Type, shape::Tuple, shift=true, unitary=true)
 
 returns an operator which performs an FFT on Arrays of type T
 
@@ -9,20 +27,41 @@ returns an operator which performs an FFT on Arrays of type T
 * `T::Type`       - type of the array to transform
 * `shape::Tuple`  - size of the array to transform
 * (`shift=true`)  - if true, fftshifts are performed
+* (`unitary=true`)  - if true, FFT is normalized such that it is unitary
 """
-function FFTOp(T::Type, shape::Tuple, shift=true)
+function FFTOp(T::Type, shape::Tuple; shift=true, unitary=true)
   plan = plan_fft(zeros(T, shape);flags=FFTW.MEASURE)
   iplan = plan_ifft(zeros(T, shape);flags=FFTW.MEASURE)
+  if unitary
+    facF = 1.0/sqrt(prod(shape))
+    facB = sqrt(prod(shape))
+  else
+    facF = 1.0
+    facB = prod(shape)
+  end
 
   if shift
-    return LinearOperator(prod(shape), prod(shape), false, false
-              , x->vec(fftshift(plan*fftshift(reshape(x,shape))))/sqrt(prod(shape))
+    return FFTOp{T}(prod(shape), prod(shape), false, false
+              , x->vec(fftshift(plan*fftshift(reshape(x,shape))))*facF
               , nothing
-              , y->vec(ifftshift(iplan*ifftshift(reshape(y,shape)))) * sqrt(prod(shape)) )
+              , y->vec(ifftshift(iplan*ifftshift(reshape(y,shape))))*facB
+              , 0, 0, 0
+              , plan
+              , iplan
+              , shift
+              , unitary)
   else
-    return LinearOperator(prod(shape), prod(shape), false, false
-            , x->vec(plan*(reshape(x,shape)))/sqrt(prod(shape))
+    return FFTOp{T}(prod(shape), prod(shape), false, false
+            , x->vec(plan*(reshape(x,shape)))*facF
             , nothing
-            , y->vec(iplan*(reshape(y,shape))) * sqrt(prod(shape)) )
+            , y->vec(iplan*(reshape(y,shape)))*facB 
+            , plan
+            , iplan
+            , shift
+            , unitary)
   end
+end
+
+function Base.copy(S::FFTOp{T}) where T
+  return FFTOp(T, size(S.plan); shift=S.shift, unitary=S.unitary)
 end
