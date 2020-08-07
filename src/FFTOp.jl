3f4af3c63955fdd1ed209f0c5e681fc03a391959
migrosser <mi.grosser@uke.de>
2020-08-07 17:05:00 +0200

    fix FFTOp

--- src/FFTOp.jl
+++ src/FFTOp.jl
@@ -1,7 +1,7 @@
 export FFTOp
 import Base.copy
 
-mutable struct FFTOp{T} <: AbstractLinearOperator{T}
+mutable struct FFTOp <: AbstractLinearOperator{ComplexF64}
   nrow :: Int
   ncol :: Int
   symmetric :: Bool
@@ -29,7 +29,7 @@ returns an operator which performs an FFT on Arrays of type T
 * (`shift=true`)  - if true, fftshifts are performed
 * (`unitary=true`)  - if true, FFT is normalized such that it is unitary
 """
-function FFTOp(T::Type, shape::Tuple; shift=true, unitary=true)
+function FFTOp(T::Type, shape::Tuple, shift=true; unitary=true)
   plan = plan_fft(zeros(T, shape);flags=FFTW.MEASURE)
   iplan = plan_ifft(zeros(T, shape);flags=FFTW.MEASURE)
   if unitary
@@ -41,7 +41,7 @@ function FFTOp(T::Type, shape::Tuple; shift=true, unitary=true)
   end
 
   if shift
-    return FFTOp{T}(prod(shape), prod(shape), false, false
+    return FFTOp(prod(shape), prod(shape), false, false
               , x->vec(fftshift(plan*fftshift(reshape(x,shape))))*facF
               , nothing
               , y->vec(ifftshift(iplan*ifftshift(reshape(y,shape))))*facB
@@ -51,10 +51,11 @@ function FFTOp(T::Type, shape::Tuple; shift=true, unitary=true)
               , shift
               , unitary)
   else
-    return FFTOp{T}(prod(shape), prod(shape), false, false
+    return FFTOp(prod(shape), prod(shape), false, false
             , x->vec(plan*(reshape(x,shape)))*facF
             , nothing
             , y->vec(iplan*(reshape(y,shape)))*facB 
+            , 0, 0, 0
             , plan
             , iplan
             , shift
@@ -62,6 +63,6 @@ function FFTOp(T::Type, shape::Tuple; shift=true, unitary=true)
   end
 end
 
-function Base.copy(S::FFTOp{T}) where T
-  return FFTOp(T, size(S.plan); shift=S.shift, unitary=S.unitary)
+function Base.copy(S::FFTOp) where T
+  return FFTOp(T, size(S.plan), S.shift, unitary=S.unitary)
 end
