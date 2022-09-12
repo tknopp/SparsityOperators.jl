03b8d516fb7d722c7bfb3298d70c34c5551ca07f
migrosser <mi.grosser@uke.de>
2022-09-12 14:29:43 +0200

    reduce allocations in FFTOp

--- src/FFTOp.jl
+++ src/FFTOp.jl
@@ -36,69 +36,59 @@ returns an operator which performs an FFT on Arrays of type T
 * (`shift=true`)  - if true, fftshifts are performed
 * (`unitary=true`)  - if true, FFT is normalized such that it is unitary
 """
-function FFTOp(T::Type, shape::Tuple, shift=true; unitary=true, cuda::Bool=false)
-  if cuda
-    @assert CUDA.functional()==true "a functional CUDA setup is required when using the option `cuda=true` in FFTOp"
-    plan = plan_fft(CuArray{T}(undef,shape))
-    iplan = plan_ifft(CuArray{T}(undef,shape))
-  else
-    plan = plan_fft(zeros(T, shape);flags=FFTW.MEASURE)
-    iplan = plan_ifft(zeros(T, shape);flags=FFTW.MEASURE)
-  end
+function FFTOp(T::Type, shape::NTuple{D,Int64}, shift::Bool=true; unitary::Bool=true, cuda::Bool=false) where D
+  
+  tmpVec = cuda ? CuArray{T}(undef,shape) : Array{Complex{real(T)}}(undef, shape)
+  plan = plan_fft!(tmpVec; flags=FFTW.MEASURE)
+  iplan = plan_bfft!(tmpVec; flags=FFTW.MEASURE)
+
   if unitary
     facF = T(1.0/sqrt(prod(shape)))
-    facB = T(sqrt(prod(shape)))
+    facB = T(1.0/sqrt(prod(shape)))
   else
     facF = T(1.0)
-    facB = T(prod(shape))
+    facB = T(1.0)
   end
 
-  function fft_multiply(res, plan, x, α, β::T2, shape, T::Type, factor) where {T2}
-    if β == zero(T2)
-      res .= (factor*α) .* vec(plan*reshape(x,shape))
-    else
-      res .= (factor*α) .* vec(plan*reshape(x,shape)) .+ β .* q
-    end
-  end
-
-  function fft_multiply_shift(res, plan, x, α, β::T2, shape, T::Type, factor) where {T2}
-    if β == zero(T2)
-      res .= (factor*α) .* vec(fftshift(plan*fftshift(reshape(x,shape))))
-    else
-      res .= (factor*α) .* vec(fftshift(plan*fftshift(reshape(x,shape)))) .+ β .* q
-    end
-  end
-
-  function fft_multiply_ishift(res, plan, x, α, β::T2, shape, T::Type, factor) where {T2}
-    if β == zero(T2)
-      res .= (factor*α) .* vec(ifftshift(iplan*ifftshift(reshape(x,shape))))
-    else
-      res .= (factor*α) .* vec(ifftshift(iplan*ifftshift(reshape(x,shape)))) .+ β .* q
-    end
-  end
+  let shape_=shape, plan_=plan, iplan_=iplan, tmpVec_=tmpVec, facF_=facF, facB_=facB
 
   if shift
     return FFTOp{T}(prod(shape), prod(shape), false, false
-              , (res, x, α, β) -> fft_multiply_shift(res, plan, x, α, β, shape, T, facF) 
+              , (res, x) -> fft_multiply_shift!(res, plan_, x, shape_, facF_, tmpVec_) 
               , nothing
-              , (res, x, α, β) -> fft_multiply_ishift(res, iplan, x, α, β, shape, T, facB) 
-              , 0, 0, 0, true, true, true, T[], T[]
+              , (res, x) -> fft_multiply_shift!(res, iplan_, x, shape_, facB_, tmpVec_) 
+              , 0, 0, 0, true, false, true, T[], T[]
               , plan
               , iplan
               , shift
               , unitary)
   else
     return FFTOp{T}(prod(shape), prod(shape), false, false
-            , (res, x, α, β) -> fft_multiply(res, plan, x, α, β, shape, T, facF) 
+            , (res, x) -> fft_multiply!(res, plan_, x, facF_, tmpVec_) 
             , nothing
-            , (res, x, α, β) -> fft_multiply(res, iplan, x, α, β, shape, T, facB)
-            , 0, 0, 0, true, true, true, T[], T[]
+            , (res, x) -> fft_multiply!(res, iplan_, x, facB_, tmpVec_)
+            , 0, 0, 0, true, false, true, T[], T[]
             , plan
             , iplan
             , shift
             , unitary)
   end
+  end
 end
+
+function fft_multiply!(res::Vector{T}, plan::P, x::Vector{Tr}, factor::T, tmpVec::Array{T,D}) where {T, Tr, P<:AbstractFFTs.Plan, D}
+  tmpVec[:] .= x
+  plan * tmpVec
+  res .= factor .* vec(tmpVec)
+end
+
+function fft_multiply_shift!(res::Vector{T}, plan::P, x::Vector{Tr}, shape::NTuple{D}, factor::T, tmpVec::Array{T,D}) where {T, Tr, P<:AbstractFFTs.Plan, D}
+  ifftshift!(tmpVec, reshape(x,shape))
+  plan * tmpVec
+  fftshift!(reshape(res,shape), tmpVec)
+  res .*= factor
+end
+
 
 function Base.copy(S::FFTOp)
   return FFTOp(eltype(S), size(S.plan), S.shift, unitary=S.unitary)
